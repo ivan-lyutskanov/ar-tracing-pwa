b9fbaf4901b83b26219cc2555ac6c5f99771f45c
Ivan Lyutskanov <ivan.luckanov@gmail.com>
2026-07-09 10:42:28 +0300

    Initial commit: AR Tracing PWA

---
+++ sw.js
@@ -0,0 +1,21 @@
+const CACHE_NAME = 'ar-trace-v1';
+const CACHE_URLS = [
+  './index.html',
+  './manifest.json'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(CACHE_URLS);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      return cachedResponse || fetch(event.request);
+    })
+  );
+});
